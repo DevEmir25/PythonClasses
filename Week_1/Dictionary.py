e13bc9769451597c224ebf0c39dcece3831f05d8
Emir Can Öz <87166160+DevEmir25@users.noreply.github.com>
2022-10-06 19:19:53 +0300

    completetion

--- Week_1/Dictionary.py
+++ Week_1/Dictionary.py
@@ -4,3 +4,19 @@ dictionary = {
     "year" : "2010"
 }
 
+print(dictionary)
+print(dictionary["brand"])
+print(dictionary["model"])
+print(dictionary["year"])
+
+dictionary["colour"] = "black"
+print(dictionary)
+print(dictionary["colour"])
+dictionary["colour"] = "white"
+print(dictionary)
+
+print(dictionary.keys())
+print(dictionary.values())
+
+for i in dictionary.keys():
+    print(i, ":", dictionary[i])
--- Week_1/Random_Number_Generator.py
+++ Week_1/Random_Number_Generator.py
@@ -1,5 +1,4 @@
 from random import randrange
-from secrets import randbelow
 
 randomNumber = randrange(1, 100) #Generate random number for 1 to 100
 print(randomNumber)
