a79b772f7a5cc2adf3ddde18816cae99311bc70e
Emir Can Öz <87166160+DevEmir25@users.noreply.github.com>
2022-10-06 14:33:36 +0300

    first commit

---
+++ Class_1_Types.py
@@ -0,0 +1,13 @@
+a = 5
+b = None
+c = 1.5
+d = "asdsad"
+e = 'e'
+f = True
+
+h = [1, 2] #list
+i = {a : "asd", d : "df"} #dictionary
+j = (1, 2, 3) #tuple (constant array)
+
+print("a :", type(a))
+print("b :", type(b))
---
+++ Class_1_Variables.py
@@ -0,0 +1,24 @@
+a = 5
+b = None
+c = 1.5
+d = -12313123214
+print(c)
+
+e = True
+print(e)
+print(a, b, c, d, e) #Correct write must like this. (Blank between charachters)
+
+print(a,b,c,d,e, sep = " ,")
+
+print(a, b, end = "")
+print(" ayni satirdayim")
+
+print(a); print(b); print(c)
+
+a, b, c = 8, 6, 9
+print(a, b, c)
+
+t = "asd"
+f = 'qwer'
+print(t, f)
+print(t); print(f);
